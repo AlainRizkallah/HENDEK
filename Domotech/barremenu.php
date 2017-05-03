ef7203e4925ac36f76c2fbd07f85dac7dd15f4a0
AlainRizkallah <alain.rizkallah@isep.fr>
2017-05-03 09:28:31 +0200

    decoupage
    decoupage de la page mère (login footer barremenu)

---
+++ Domotech/barremenu.php
@@ -0,0 +1,9 @@
+<ul class="barremenu">
+  <li> <a href =".html" > Mon espace </a> </li>
+  <li> <a href =".html" > S'abonner </a> </li>
+  <li> <a href =".html" > Notre solution </a> </li>
+  <li> <a href =".html" > Actualites </a> </li>
+  <li> <a href =".html" > Nous contacter </a> </li>
+  <li> <a href =".html" > Qui sommes nous? </a> </li>
+  <li> <input type="search" placeholder="Recherche"> </li>
+</ul>
