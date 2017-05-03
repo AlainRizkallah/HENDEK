6cc727a0589585829d12dc30d6c89e9e8613e5d2
NicolasKritter <nicolas.kritter@isep.fr>
2017-05-03 09:43:06 +0200

    maj index.php & clean
    supression de fichiers inutiles et creation du fichier index

--- Domotech/index.php
+++ Domotech/index.php
@@ -1,4 +1,27 @@
 <!DOCTYPE html>
-<link rel="stylesheet" href="./Styles/style.css" >
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="Styles/HomePage.css" >
+  <title>Domotech</title>
+</head>
+  <body>
+    <header>
+    <h1 class="titre">Domotech</h1>
 
-<?php include('Vue/footer.html') ?>
+<?php include('Vue/login.php'); ?>
+
+</header>
+
+    <?php include("Vue/barremenu.php");?>
+
+    <section>
+        <p>
+           Some text
+        </p>
+    </section>
+
+<?php include("Vue/footer.html"); ?>
+
+  </body>
+</html>
