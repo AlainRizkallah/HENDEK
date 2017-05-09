99a7b7109d45fd6b1804005527c1e5a56eba11b7
NicolasKritter <nicolas.kritter@isep.fr>
2017-05-09 08:42:02 +0200

    Changement de texte

--- Domotech/Vue/barremenu.php
+++ Domotech/Vue/barremenu.php
@@ -1,8 +1,8 @@
 <ul class="barremenu">
   <li> <a href =".html" > Mon espace </a> </li>
-  <li> <a href =".html" > S'abonner </a> </li>
-  <li> <a href =".html" > Notre solution </a> </li>
-  <li> <a href =".html" > Actualites </a> </li>
+  <li> <a href =".html" > Offres </a> </li>
+  <li> <a href =".html" > Solutions </a> </li>
+  <li> <a href =".html" > Actualités </a> </li>
   <li> <a href =".html" > Nous contacter </a> </li>
   <li> <a href =".html" > Qui sommes nous? </a> </li>
   <li> <input type="search" placeholder="Recherche"> </li>
