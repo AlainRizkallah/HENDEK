d7ebc819d73f85f362ca50f0c673c7567e0a4d94
NicolasKritter <nicolas.kritter@isep.fr>
2017-05-09 09:01:17 +0200

    cleanup

--- Domotech/index.php
+++ Domotech/index.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -27,40 +27,3 @@
 
   </body>
 </html>
-=======
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="Styles/HomePage.css" >
-  <title>Domotech</title>
-</head>
-  <body>
-    <header>
-    <h1 class="titre">Domotech</h1>
-    <?php include ('Controleur/session-manager.php'); ?>
-
-
-
-</header>
-
-    <?php include("Vue/barremenu.php");?>
-
-    <section>
-        <p>
-           Some text
-           <div class="image1">
-+            <img src="http://nsa37.casimages.com/img/2017/05/05/170505124535648744.png" width="100%"/>
-+            <br />
-+          </div>
-+          <div class="texte1">
-+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu tempor odio, varius fringilla sapien. Suspendisse ex augue, sodales quis sollicitudin ut, dictum at quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras vel vestibulum leo. Nullam ante dui, iaculis vitae varius vel, suscipit ut turpis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque consequat neque vehicula tincidunt rutrum. Proin nunc ligula, vestibulum ac tellus et, venenatis pellentesque nulla. Aenean finibus est nec dapibus sollicitudin. Phasellus diam nisl, scelerisque eu dapibus et, dapibus sit amet eros. Donec euismod nibh est, at ornare massa pellentesque ut. Vivamus nec maximus dui, et sodales dolor. Ut euismod efficitur purus at congue.
-+          </div>
-        </p>
-    </section>
-
-<?php include("Vue/footer.html"); ?>
-
-  </body>
-</html>
->>>>>>> refs/remotes/origin/master
