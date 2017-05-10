85d7820ca86e45939768e919527ccbcdd5f11d5b
AlainRizkallah <alain.rizkallah@isep.fr>
2017-05-10 10:45:36 +0200

    +nom prenom email pour s'inscrire

--- Domotech/Vue/register.php
+++ Domotech/Vue/register.php
@@ -2,6 +2,12 @@
   <?php  include("Controleur/addUser.php"); ?>
   <form method="POST">
       <div class="container">
+        <label><b>Nom</b></label>
+        <input type="text" placeholder="Nom" name="Nom" required>
+        <label><b>Prénom</b></label>
+        <input type="text" placeholder="Prénom" name="Prénom" required>
+        <label><b>E-mail</b></label>
+        <input type="text" placeholder="someone@example.com" name="email" required>
         <label><b>Username</b></label>
         <input type="text" placeholder="Enter Username" name="userName" required>
         <label><b>Password</b></label>
