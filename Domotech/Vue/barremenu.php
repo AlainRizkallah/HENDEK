8d93f5f49977793042f93c183b6e68f321cd097d
NicolasKritter <nicolas.kritter@isep.fr>
2017-05-12 10:46:17 +0200

    barre menu s'adapte automatiquement

--- Domotech/Vue/barremenu.php
+++ Domotech/Vue/barremenu.php
@@ -1,3 +1,18 @@
+<?php
+if(isset($_SESSION["userID"])){ // est connecté
+echo('
+<link rel="stylesheet" href="Styles/monespace.css" >
+<ul class="menuclient">
+        <li> <a href ="monespace/mamaison.php" > Ma Maison </a> </li>
+        <li> <a href ="monespace/capteurs.php" > Capteurs </a> </li>
+        <li> <a href ="monespace/ajoutcapteurs.php" > Ajouter des capteurs </a> </li>
+        <li> <a href ="monespace/consommation.php" > Consommation </a> </li>
+        <li> <a href ="monespace/utilisateurs.php" > Utilisateurs </a> </li>
+        <li> <a href ="monespace/piece.php" > Pièce </a> </li>
+        <li> <a href ="monespace/effecteurs.php" > Effecteurs </a> </li>
+      </ul>');
+} else {
+echo('
 <ul class="barremenu">
   <li> <a href ="monespace.php" > Mon espace </a> </li>
   <li> <a href =".html" > Offres </a> </li>
@@ -6,4 +21,6 @@
   <li> <a href =".html" > Nous contacter </a> </li>
   <li> <a href =".html" > Qui sommes nous? </a> </li>
   <li> <input type="search" placeholder="Recherche"> </li>
-</ul>
+</ul>');
+}
+?>
