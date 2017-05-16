a7b488ee41ceeaedafd2e2d61ea20bd05a350d43
AlainRizkallah <alain.rizkallah@isep.fr>
2017-05-16 09:54:37 +0200

    aba

--- Domotech/monespace.php
+++ Domotech/monespace.php
@@ -15,24 +15,18 @@
       header('Location:login.php ');
       die;
     }
-
       ?>
-
-
 
 </header>
 
      <?php include("Vue/barremenu.php");?>
 
-
 <section>
   <?php if(isset($_GET['cible'])) { // on regarde la page où il veut aller
       if($_GET['cible'] == 'mon-espace/ma-maison'){// Changer les cibles
           include("Vue/login.php");
-      } else if ($_GET['cible'] == "etape1"){
-          include("Modele/utilisateurs.php");
-          $reponse = utilisateurs($db);
-          include("Vue/etape1.php");
+      } else if ($_GET['cible'] == "monespace/capteurs.php"){
+          include("Vue/monespace/capteurs.php");
       } else if ($_GET['cible'] == "etape2"){
           include("Vue/etape2.php");
       } else if ($_GET['cible'] == "etape3"){
