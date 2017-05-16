36b392ee308a295f43a36355f30f9c49273eb334
Thibault HENTGES <thibault.hentges@gmail.com>
2017-05-17 00:40:36 +0200

    Selection de capteur - formulaire + css

--- Domotech/Vue/monespace/capteurs.php
+++ Domotech/Vue/monespace/capteurs.php
@@ -1,8 +1,8 @@
-Exemple double formulaires :
+<!-- MODE AVEC DEUX FORMULAIRES
 <ul>
 
   <li>
-<form method="post" action=""> <!-- insérer dans le champs action la page vers laquelle est redirigé l'utilisateur lors de l'envoi du formulaire -->
+<form method="post" action="">
    <p>
        <label for="piece">Choisissez une pièce</label><br />
        <select name="piece" id="piece">
@@ -16,7 +16,7 @@ Exemple double formulaires :
  </li>
 
  <li>
-<form method="post" action=""> <!-- insérer dans le champs action la page vers laquelle est redirigé l'utilisateur lors de l'envoi du formulaire -->
+<form method="post" action="">
   <p>
       <label for="capteur">Choisissez un capteur</label><br />
       <select name="capteur" id="capteur">
@@ -29,20 +29,44 @@ Exemple double formulaires :
 
 </ul>
 
-<br> <br> <br> <br>
-Exemple formulaire 2 en 1.
+-->
 
-<form method="post" action="">
+<form class ="formCapteurs "method="post" action=""> <!-- insérer dans le champs action la page vers laquelle est redirigé l'utilisateur lors de l'envoi du formulaire -->
    <p>
-       <label for="capteur">Choisissez un capteur</label><br />
+      <div class="titreFormCapteurs" <label for="capteur">&nbsp Selectionnez un capteur &nbsp</label><br /> </div>
+       <br><br>
+
        <select name="capteur" id="capteur">
-           <optgroup label="Chambre enfant">
-               <option value="temp1">Temperature</option>
-               <option value="humidite1">Humidité</option>
+           <optgroup label="Chambre">  <!-- ASSOCIER ID 1 A LA PREMIERE OPTION dans la BDD -->
+               <option value="lum1">Luminosité</option>
+               <option value="temp1">Température</option>
            </optgroup>
-           <optgroup label="Chambre parentale">
-               <option value="temp2">Temperature</option>
-               <option value="humidite2">Humidité</option>
+           <optgroup label="Bureau"> <!-- ASSOCIER ID 2 A LA PREMIERE OPTION dans la BDD -->
+             <option value="vid2">Vidéosurveillance</option>
+             <option value="lum2">Luminosité</option>
+             <option value="pres2">Présence</option>
+             <option value="temp2">Température</option>
+           </optgroup>
+            <optgroup label="Salon">
+              <option value="vid3">Vidéosurveillance</option>
+              <option value="lum3">Luminosité</option>
+              <option value="pres3">Présence</option>
+              <option value="temp3">Température</option>
+           </optgroup>
+           <optgroup label="Salle de Bain">
+             <option value="lum4">Luminosité</option>
+             <option value="pres4">Présence</option>
+             <option value="humid4">Humidité</option>
+             <option value="temp4">Température</option>
+          </optgroup>
+          <optgroup label="Entrée">
+              <option value="vid5">Vidéosurveillance</option>
+              <option value="lum5">Luminosité</option>
+              <option value="pres5">Présence</option>
+              <option value="humid5">Humidité</option>
+              <option value="temp5">Température</option>
+          </optgroup>
+          <input type="submit" value="Envoyer" />
        </select>
    </p>
 </form>
