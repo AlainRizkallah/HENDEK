317f152c91de99d3ce4a2a6d5b8f39961e43f31c
NicolasKritter <nicolas.kritter@isep.fr>
2017-05-17 20:50:01 +0200

    MAj graphique du meny

--- Domotech/Vue/barremenu.php
+++ Domotech/Vue/barremenu.php
@@ -1,25 +1,28 @@
 <?php
 if(isset($_SESSION["userID"])){ // est connecté
 echo('
-<ul class="barremenu">
-        <li> <a class="barre-menu-element" href ="?cible=mon-espace/ma-maison" > Ma Maison </a> </li>
-        <li> <a class="barre-menu-element" href ="?cible=monespace/capteurs.php" > Capteurs </a> </li>
-        <li> <a class="barre-menu-element" href ="?cible=monespace/ajoutcapteurs.php" > Ajouter des capteurs </a> </li>
-        <li> <a class="barre-menu-element" href ="?cible=monespace/consommation.php" > Consommation </a> </li>
-        <li> <a class="barre-menu-element" href ="?cible=monespace/utilisateurs.php" > Utilisateurs </a> </li>
-        <li> <a class="barre-menu-element" href ="?cible=monespace/piece.php" > Pièce </a> </li>
-        <li> <a class="barre-menu-element" href ="?cible=monespace/effecteurs.php" > Effecteurs </a> </li>
-      </ul>');
+<div class="barremenu">
+        <a class="barre-menu-element" href ="index.php" > Acceuil </a>
+         <a class="barre-menu-element" href ="?cible=mon-espace/ma-maison" > Ma Maison </a>
+         <a class="barre-menu-element" href ="?cible=monespace/capteurs.php" > Capteurs </a>
+         <a class="barre-menu-element" href ="?cible=monespace/ajoutcapteurs.php" > Ajouter des capteurs </a>
+         <a class="barre-menu-element" href ="?cible=monespace/consommation.php" > Consommation </a>
+         <a class="barre-menu-element" href ="?cible=monespace/utilisateurs.php" > Utilisateurs </a>
+         <a class="barre-menu-element" href ="?cible=monespace/piece.php" > Pièce </a>
+         <a class="barre-menu-element" href ="?cible=monespace/effecteurs.php" > Effecteurs </a>
+      </div>');
 } else {
 echo('
-<ul class="barremenu">
-  <li> <a class="barre-menu-element" href ="monespace.php" > Mon espace </a> </li>
-  <li> <a class="barre-menu-element" href =".html" > Offres </a> </li>
-  <li> <a class="barre-menu-element" href =".html" > Solutions </a> </li>
-  <li> <a class="barre-menu-element" href =".html" > Actualités </a> </li>
-  <li> <a class="barre-menu-element" href =".html" > Nous contacter </a> </li>
-  <li> <a class="barre-menu-element" href  =".html" > Qui sommes nous? </a> </li>
-  <li class="barre-menu-element"> <input  type="search" placeholder="Recherche"> </li>
-</ul>');
+<div class="barremenu">
+    <a class="barre-menu-element" href ="index.php" > Acceuil </a>
+    <a class="barre-menu-element" href ="monespace.php" > Mon espace </a>
+    <a class="barre-menu-element" href =".html" > Offres </a>
+    <a class="barre-menu-element" href =".html" > Solutions </a>
+    <a class="barre-menu-element" href =".html" > Actualités </a>
+    <a class="barre-menu-element" href =".html" > Nous contacter </a>
+    <a class="barre-menu-element" href  =".html" > Qui sommes nous? </a>
+   </div>');
 }
+
+//<input  class="barre-menu-element" type="search" placeholder="Recherche">
 ?>
