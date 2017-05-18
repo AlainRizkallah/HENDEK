11f991be9d9b9c246148eacb382bcdc35da1bd44
Thibault HENTGES <thibault.hentges@gmail.com>
2017-05-18 22:13:31 +0200

    redirection après ajout de données à la BDD

--- Domotech/Controleur/addCapteur.php
+++ Domotech/Controleur/addCapteur.php
@@ -3,5 +3,5 @@
 include("../Modele/db-capteur-manager.php");
 $resultat = addCapteur($db,$_POST['piece'],$_POST['maison'],$_POST['type']);
 echo ($resultat);
-
+header ("Location: $_SERVER[HTTP_REFERER]" ); // redirige l'utilisateur sur la page précédente
 ?>
