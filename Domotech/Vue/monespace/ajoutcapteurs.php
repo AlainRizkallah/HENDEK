a02b0a6681a6117d1c988dd3f343ee647ca4cc59
Thibault HENTGES <thibault.hentges@gmail.com>
2017-05-19 04:19:49 +0200

    ajout liste des capteurs sur page ajoutcapteurs

--- Domotech/Vue/monespace/ajoutcapteurs.php
+++ Domotech/Vue/monespace/ajoutcapteurs.php
@@ -1,6 +1,22 @@
 <!-- TODO ajouter ici liste des capteurs présents dans la BDD -->
-
 <div class=textecentre>
+
+<p class="boxtitle">
+  &nbsp Liste des capteurs &nbsp
+</p><br>
+
+<?php
+include("Modele/db-capteur-manager.php");
+$reponse = $db->query('SELECT * FROM capteur');
+$donnees = $reponse->fetch();
+while ($donnees = $reponse->fetch()) {
+  echo $donnees['type'];?> dans la salle <?php echo $donnees['idSalle'] ?>
+
+  <br> <?php }
+$reponse->closeCursor();
+?>
+
+
   <p class="boxtitle">
     &nbsp Ajouter un capteur &nbsp
   </p>
@@ -15,21 +31,21 @@
      <p class="textegauche">
          <label for="piece">Choisissez une pièce</label><br />
          <select name="piece">
-             <option value="0">Chambre</option>
-             <option value="1">Bureau</option>
-             <option value="2">Salon</option>
-             <option value="3">Salle de Bain</option>
-             <option value="4">Entrée</option>
+             <option value="0">Salle 0</option>
+             <option value="1">Salle 1</option>
+             <option value="2">Salle 2</option>
+             <option value="3">Salle 3</option>
+             <option value="4">Salle 4</option>
          </select>
      </p>
      <p class="textedroite">
          <label for="type">Choisissez un type de capteur</label><br />
          <select name="type">
-             <option value="vid">Vidéosurveillance</option>
-             <option value="lum">Luminosité</option>
-             <option value="pres">Présence</option>
-             <option value="humid">Humidité</option>
-             <option value="temp">Température</option>
+             <option value="Vidéosurveillance">Vidéosurveillance</option>
+             <option value="Luminosité">Luminosité</option>
+             <option value="Présence">Présence</option>
+             <option value="Humidité">Humidité</option>
+             <option value="Température">Température</option>
          </select>
      </p>
      <input type="submit" value="ajouter un capteur" name=addCapteur />
