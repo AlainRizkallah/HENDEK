1312a30a06752598826be9aa7985f5ea70803bf4
Thibault HENTGES <thibault.hentges@gmail.com>
2017-05-22 03:49:40 +0200

    contenu statique de page "actualité" admin

---
+++ Domotech/Vue/accueiladmin/news.php
@@ -0,0 +1,18 @@
+<form method="post" action="">
+    <p>
+        <label for="titrenews" >Titre</label> : <input type="text" name="titrenews" />
+    </p>
+    <p>
+       <label for="contenunews">
+       Contenu de l'actualité :
+       </label>
+       <br /><br />
+
+       <textarea name="contenunews" rows="30" cols="209">
+       Inscrivez ici le texte contenu dans votre actualité
+       </textarea>
+    </p>
+    <p>
+        <input type="submit" value="mettre l'actualité en ligne"/>
+    </p>
+</form>
