3947093ca22f0cceae643ec7dc971071cf65c064
Thibault HENTGES <thibault.hentges@gmail.com>
2017-05-23 15:33:44 +0200

    Revert "bis"
    This reverts commit 2f92e547a7734bf4b8252a0f74c372e42896d1c9.

--- Domotech/Vue/listemaisons.php
+++ Domotech/Vue/listemaisons.php
@@ -1,5 +1,5 @@
 <?php
-include("Modele/db-maison-manager.php"); 
+//  $reponse = $db->query('SELECT * FROM habitation');
 $reponse = getHabitationsList($db,  $_SESSION["idGroupe"]);
   while ($donnees = $reponse->fetch()) {
     echo $donnees['nom'];?> de superficie <?php echo $donnees['superficie'];?>m² à l'adresse <?php echo $donnees['adresse'];?>
--- Domotech/Vue/monespace/accueilclient.php
+++ Domotech/Vue/monespace/accueilclient.php
@@ -1,3 +1,5 @@
+<?php include("Modele/db-maison-manager.php"); ?>
+
 <div class=center>
 <p class="boxtitle"> BIENVENUE SUR VOTRE ESPACE
 </p></div>
