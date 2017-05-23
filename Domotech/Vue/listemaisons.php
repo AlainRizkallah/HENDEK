a24e3e55569fb33dbb5c7ec7b2c61ac9306d8cdf
Thibault HENTGES <thibault.hentges@gmail.com>
2017-05-23 15:13:49 +0200

    affichage liste maison par groupe

--- Domotech/Vue/listemaisons.php
+++ Domotech/Vue/listemaisons.php
@@ -1,5 +1,6 @@
 <?php
-  $reponse = $db->query('SELECT * FROM habitation');
+//  $reponse = $db->query('SELECT * FROM habitation');
+$reponse = getHabitationsList($db,  $_SESSION["idGroupe"]);
   while ($donnees = $reponse->fetch()) {
     echo $donnees['nom'];?> de superficie <?php echo $donnees['superficie'];?>m² à l'adresse <?php echo $donnees['adresse'];?>
     <br> <?php }
