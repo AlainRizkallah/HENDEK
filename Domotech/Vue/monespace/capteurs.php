d7f528bc5f708f370056b81488698a8ebaad3f1e
NicolasKritter <nicolas.kritter@isep.fr>
2017-05-24 11:41:23 +0200

    Selection Maison dans menu

--- Domotech/Vue/monespace/capteurs.php
+++ Domotech/Vue/monespace/capteurs.php
@@ -3,8 +3,12 @@ include_once("Modele/db-maison-manager.php");
 include_once("Modele/db-salle-manager.php");
 
  ?>
-<script  async defer src="js/showElement.js"></script>
-<?php   $resultat=getSalleList($db, $_SESSION['idGroupe']);//Mettre idMaison
+<script  async defer src="js/communicationPhp.js"></script>
+<?php
+if(isset($_SESSION['idMaison'])){
+  $resultat=getSalleList($db, $_SESSION['idMaison']);
+}
+
 ?>
 <div class="dropdown left" >
       <button class="boutton">Sélection de la Salle:</button>
@@ -13,7 +17,7 @@ include_once("Modele/db-salle-manager.php");
 
           <?php
             while ($liste=$resultat->fetch()){?>
-              <button onclick= showCapteurs(<?php echo($liste['ID']);?>,'Controleur/display-capteur.php') class="boutton"><?php echo($liste['sal']);?></button>
+              <button onclick= envoiePhP("salle",<?php echo($liste['ID']);?>,'Controleur/display-capteur.php') class="boutton"><?php echo($liste['sal']);?></button>
           <?php } $resultat->closeCursor();?>
 
         </div>
