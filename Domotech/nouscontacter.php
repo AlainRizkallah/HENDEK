7a578178fa13e5fcc7e7fd6bf5fed764ecbce177
Lahsine-Adam <adam.lahsine@isep.fr>
2017-05-26 10:57:39 +0200

    1

---
+++ Domotech/nouscontacter.php
@@ -0,0 +1,13 @@
+<?php include("Vue/header.php");?>
+
+    <section>
+      <div class="center">
+        <?php include ('Vue/register.php');?>
+
+      </div>
+    </section>
+
+<?php include("Vue/footer.html"); ?>
+
+  </body>
+</html>
