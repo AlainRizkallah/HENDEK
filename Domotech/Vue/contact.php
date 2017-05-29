82a7b44a48957782a5cc824e85f9f4cfc7b2b510
Lahsine-Adam <adam.lahsine@isep.fr>
2017-05-29 09:12:18 +0200

    nous contacter

--- Domotech/Vue/contact.php
+++ Domotech/Vue/contact.php
@@ -1,20 +1,20 @@
 <div class="contact">
-  <?php  include("Controleur/addUser.php"); ?>
+  <?php  include("Controleur/traiterMessage.php"); ?>
   <form method="POST">
       <div class="container">
         <label><b>Nom</b></label>
-        <input type="text" placeholder="Nom" name="nom" required>
+        <input type="text" placeholder="Nom" id="nom" name="nom" required>
         <label><b>Prénom</b></label>
-        <input type="text" placeholder="Prénom" name="prenom" required>
+        <input type="text" placeholder="Prénom" id="prenom" name="prenom" required>
         <label><b>E-mail</b></label>
-        <input type="email" placeholder="someone@example.com" name="email" required>
+        <input type="email" placeholder="someone@example.com" id="email" name="email" required>
         <label><b>Téléphone</b></label>
-        <input type="tel" placeholder="votre numéro de téléphone" name="tel" required>
+        <input type="tel" placeholder="votre numéro de téléphone" id="tel" name="tel" >
         <label><b>Message</b></label>
-        <input type="text" placeholder="Votre message : " name="message" required>
+        <input type="text" placeholder="Objet " id="objet" name="objet" required>
+        <textarea id="message" placeholder="Votre message " name="message" tabindex="4" cols="79" rows="8"></textarea>
 
-
-        <button type="submit" name="btnRegister">Créer le compte</button>
+        <button type="submit" name="envoi">Envoyer</button>
       </div>
     </form>
   </div>
