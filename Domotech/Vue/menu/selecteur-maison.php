799284751ff9b13363f5e21f7553152b7ec5b0cd
NicolasKritter <nicolas.kritter@isep.fr>
2017-05-31 10:53:37 +0200

    dropdown choix de maison

--- Domotech/Vue/menu/selecteur-maison.php
+++ Domotech/Vue/menu/selecteur-maison.php
@@ -3,9 +3,16 @@ include_once("Modele/db-maison-manager.php");
 
 $reponse = getHabitationsList($db,  $_SESSION["idGroupe"]);
 //TODO personnaliser la fonction dans le onClick
+
+if(!isset($_SESSION["nomMaison"])){
+  $nomMaison = "Choix de la Maison ▼";
+}else{
+    $nomMaison = $_SESSION["nomMaison"];
+}
+
  ?>
 <div class="dropdown" >
-      <button class="boutton">Choix de la Maison ▼</button>
+      <button class="boutton"><?php echo $nomMaison ?></button>
 
         <div class="dropdown-content">
 
@@ -14,9 +21,12 @@ $reponse = getHabitationsList($db,  $_SESSION["idGroupe"]);
               if(!isset( $_SESSION["idMaison"])){
                 $_SESSION["idMaison"] = $liste['ID'];
               }
-              $params = "maisonId=".$liste['ID'];
+              $nom =  str_replace(" ","_",$liste['nom']);
+              $params = "maisonId=".$liste['ID']."&nomMaison=".$nom;
+              if($nomMaison!=$liste['nom']){
               ?>
               <button onclick= envoiePhP("<?php echo($params);?>",'Controleur/maison-manager.php') class="boutton"><?php echo($liste['nom']);?></button>
+              <?php }?>
           <?php } $reponse->closeCursor();?>
 
         </div>
