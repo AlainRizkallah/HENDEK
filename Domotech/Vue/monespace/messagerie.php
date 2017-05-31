134412c2b2f80101a453c19febf2e541dcc86917
Thibault HENTGES <thibault.hentges@gmail.com>
2017-05-31 12:00:40 +0200

    bouton réponse messagerie interne

--- Domotech/Vue/monespace/messagerie.php
+++ Domotech/Vue/monespace/messagerie.php
@@ -6,7 +6,7 @@ include_once("Modele/db-message-manager.php");
   <form method="POST" action= 'Controleur/message-manager.php'>
       <div class="container">
 
-        <input type="text" placeholder="Identifiant du destinataire" name="destinataire" required>
+        <input type="text" placeholder="Identifiant du destinataire" name="destinataire" id=destinataire value="" required>
         <input type="text" placeholder="Objet" name="objet" required>
         <textarea id="message" placeholder="Votre message" name="message" tabindex="4" cols="120" rows="8"></textarea></br></br>
 
