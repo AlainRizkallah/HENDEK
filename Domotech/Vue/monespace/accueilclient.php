0d4be869dcbb3abf55439b24ca5ba515158cecbd
Thibault HENTGES <thibault.hentges@gmail.com>
2017-06-02 16:41:51 +0200

    indentation propre page accueilclient

--- Domotech/Vue/monespace/accueilclient.php
+++ Domotech/Vue/monespace/accueilclient.php
@@ -2,56 +2,47 @@
 include_once("Modele/db-salle-manager.php");
 include_once("Modele/db-capteur-manager.php");?>
 
-<div class=center>
+<div class="conteneurBloc n2 center">
 <h3> BIENVENUE SUR VOTRE ESPACE
 </h3></div>
 
-
-
+<div class="conteneurBloc n2 left"></br>
   <h2>
   Liste de vos maisons
-</h2>
-<div class="conteneurBloc n2 left"></br>
-    <?php include('Vue/Listes/listemaisons.php');?>
+  </h2>
+  <?php include('Vue/Listes/listemaisons.php');?>
 </div>
 
-    <div class="conteneurBloc n2 right"><br>
-      <p class=center>
+<div class="conteneurBloc n2 right"><br>
+  <p class=center>
     <a class="boutton" href ="monespace.php?cible=monespace/mamaison.php" >ajouter/supprimer des maisons</a>
   </p>
 </div>
 
+<br><br><br>
 
-</br>
-
-
-  <br><br>
-
+<div class="conteneurBloc n2 left"></br>
   <h2>
     Liste de vos pièces
   </h2>
-  <div class="conteneurBloc n2 left"></br>
-    <?php include('Vue/Listes/listepieces.php');?>
-  </div>
-    <div class="conteneurBloc n2 right"><br>
-      <p class=center>
-      <a class="boutton" href ="monespace.php?cible=monespace/piece.php" >ajouter/supprimer des pièces</a>
-    </p>
-  </div>
+  <?php include('Vue/Listes/listepieces.php');?>
+</div>
+<div class="conteneurBloc n2 right"><br>
+  <p class=center>
+    <a class="boutton" href ="monespace.php?cible=monespace/piece.php" >ajouter/supprimer des pièces</a>
+  </p>
+</div>
 
-  <br>
+  <br><br><br>
 
-
-  <br><br>
-
+<div class="conteneurBloc n2 left"><br>
   <h2>
     Liste de vos capteurs
   </h2>
-  <div class="conteneurBloc n2 left"></br>
-    <?php include('Vue/Listes/listecapteurs.php');?>
-  </div>
-      <div class="conteneurBloc n2 right"><br>
-    <p class=center>
-      <a class="boutton" href ="monespace.php?cible=monespace/ajoutcapteurs.php" >ajouter/supprimer des capteurs</a>
-    </p>
-  </div>
+  <?php include('Vue/Listes/listecapteurs.php');?>
+</div>
+<div class="conteneurBloc n2 right"><br>
+  <p class=center>
+    <a class="boutton" href ="monespace.php?cible=monespace/ajoutcapteurs.php" >ajouter/supprimer des capteurs</a>
+  </p>
+</div>
