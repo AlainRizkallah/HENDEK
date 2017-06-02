fdcdd24e88f7c31fbc1b68d6774dc19737d3b8fb
Thibault HENTGES <thibault.hentges@gmail.com>
2017-06-02 17:58:21 +0200

    correction html css ACCUEIL

--- Domotech/Vue/monespace/accueilclient.php
+++ Domotech/Vue/monespace/accueilclient.php
@@ -6,39 +6,41 @@ include_once("Modele/db-capteur-manager.php");?>
 <h3> BIENVENUE SUR VOTRE ESPACE
 </h3></div>
 
-<div class="conteneurBloc n2 left"></br>
+<div class="conteneurBloc n2"></br>
   <h2>
   Liste de vos maisons
   </h2>
+</div>
+  <div class="conteneurBloc n2 right">
+    <p class=center>
+      <a class="boutton" href ="monespace.php?cible=monespace/mamaison.php" >ajouter/supprimer des maisons</a>
+    </p>
+  </div>
   <?php include('Vue/Listes/listemaisons.php');?>
-</div>
 
-<div class="conteneurBloc n2 right"><br>
-  <p class=center>
-    <a class="boutton" href ="monespace.php?cible=monespace/mamaison.php" >ajouter/supprimer des maisons</a>
-  </p>
-</div>
 
-<div style="clear:left;" class="conteneurBloc n2 left"></br>
+<div class="conteneurBloc n2"><br>
   <h2>
     Liste de vos pièces
   </h2>
+</div>
+  <div class="conteneurBloc n2 right">
+    <p class=center>
+      <a class="boutton" href ="monespace.php?cible=monespace/piece.php" >ajouter/supprimer des pièces</a>
+    </p>
+  </div>
   <?php include('Vue/Listes/listepieces.php');?>
-</div>
-<div class="conteneurBloc n2 right"><br>
-  <p class=center>
-    <a class="boutton" href ="monespace.php?cible=monespace/piece.php" >ajouter/supprimer des pièces</a>
-  </p>
-</div>
 
-<div style="clear:left;" class="conteneurBloc n2 left"><br>
+
+
+<div><br>
   <h2>
     Liste de vos capteurs
   </h2>
-  <?php include('Vue/Listes/listecapteurs.php');?>
 </div>
-<div class="conteneurBloc n2 right"><br>
+<div class="conteneurBloc n2 right">
   <p class=center>
     <a class="boutton" href ="monespace.php?cible=monespace/ajoutcapteurs.php" >ajouter/supprimer des capteurs</a>
   </p>
 </div>
+  <?php include('Vue/Listes/listecapteurs.php');?>
