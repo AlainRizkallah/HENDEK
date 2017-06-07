936758f06789f14d3730b72e832c2935e99d79bc
AlainRizkallah <alain_riz@hotmail.com>
2017-06-07 10:02:36 +0200

    panneau admin

--- Domotech/testadmin.php
+++ Domotech/testadmin.php
@@ -1,12 +1,12 @@
-  <link rel="stylesheet" href="../Vue/Styles/style1.css" >
+
 <?php
 session_start();
-include('../Modele/db-utilisateur-manager.php');
+include('Modele/db-utilisateur-manager.php');
 $reponse=userAdmin($db,$_SESSION['userID']);
 $ligne = $reponse->fetch();
 if($ligne['status']=='admin')
 {
-  include('../accueiladmin.php');
+  include('accueiladmin.php');
 }
 else{
   echo('tes pas admin connard casse toi');
