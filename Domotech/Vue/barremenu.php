9f016a2677ef0a750026a54bed03706b360cbce3
NicolasKritter <nicolas.kritter@isep.fr>
2017-06-09 21:26:54 +0200

    suppression padding

--- Domotech/Vue/barremenu.php
+++ Domotech/Vue/barremenu.php
@@ -11,11 +11,11 @@ echo('
 <div class="barremenu">
           <a class="barre-menu-element" href ="monespace.php?cible=monespace/accueilclient.php" > Accueil </a>
           <a class="barre-menu-element" href ="monespace.php?cible=monespace/mamaison.php" > Ma Maison </a>
-          <a class="barre-menu-element n8" href ="monespace.php?cible=monespace/piece.php" > Pièce </a>
-          <a class="barre-menu-element n8" href ="monespace.php?cible=monespace/ajoutcapteurs.php" > Gérer mes capteurs/effecteurs </a>
-          <a class="barre-menu-element n8 " href ="monespace.php?cible=monespace/capteurs.php" > Etat des capteurs/effecteurs</a>
-          <a class="barre-menu-element n8" href ="monespace.php?cible=monespace/utilisateurs.php" > Utilisateurs </a>
-          <a class="barre-menu-element n8" href ="monespace.php?cible=monespace/consommation.php" > Consommation </a>
+          <a class="barre-menu-element " href ="monespace.php?cible=monespace/piece.php" > Pièce </a>
+          <a class="barre-menu-element " href ="monespace.php?cible=monespace/ajoutcapteurs.php" > Gérer mes capteurs/effecteurs </a>
+          <a class="barre-menu-element  " href ="monespace.php?cible=monespace/capteurs.php" > Etat des capteurs/effecteurs</a>
+          <a class="barre-menu-element " href ="monespace.php?cible=monespace/utilisateurs.php" > Utilisateurs </a>
+          <a class="barre-menu-element " href ="monespace.php?cible=monespace/consommation.php" > Consommation </a>
 
       </div>
       ');
@@ -24,14 +24,14 @@ echo('
 echo('
 <div class="barremenu">
 
-    <a class="barre-menu-element n8" href ="." > Accueil </a>
-    <a class="barre-menu-element n8" href ="monespace.php" > Mon espace </a>
-    <a class="barre-menu-element n8" href =".html" > Offres </a>
-    <a class="barre-menu-element n8" href =".html" > Solutions </a>
-    <a class="barre-menu-element n8" href =".html" > Actualités </a>
-    <a class="barre-menu-element n8" href ="nouscontacter.php" > Nous contacter </a>
-    <a class="barre-menu-element n8" href  =".html" > Qui sommes nous? </a>
-    <a class="barre-menu-element n8" href  ="inscription.php" > S&apos;inscrire </a>
+    <a class="barre-menu-element " href ="." > Accueil </a>
+    <a class="barre-menu-element " href ="monespace.php" > Mon espace </a>
+    <a class="barre-menu-element " href =".html" > Offres </a>
+    <a class="barre-menu-element " href =".html" > Solutions </a>
+    <a class="barre-menu-element " href =".html" > Actualités </a>
+    <a class="barre-menu-element " href ="nouscontacter.php" > Nous contacter </a>
+    <a class="barre-menu-element " href  =".html" > Qui sommes nous? </a>
+    <a class="barre-menu-element " href  ="inscription.php" > S&apos;inscrire </a>
    </div>');
 }
 
