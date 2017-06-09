6586a4c667a9806b19e0d3838574265797c50a51
Thibault HENTGES <thibault.hentges@gmail.com>
2017-06-09 18:10:12 +0200

    css ACCUEILCLIENT good

--- Domotech/Vue/monespace/accueilclient.php
+++ Domotech/Vue/monespace/accueilclient.php
@@ -3,57 +3,62 @@ include_once("Modele/db-salle-manager.php");
 include_once("Modele/db-capteur-manager.php");
 include_once("Modele/db-effecteur-manager.php");?>
 
-<div class="conteneurBloc n2 center">
+<div class="conteneurBloc center">
 <h3> BIENVENUE SUR VOTRE ESPACE
 </h3></div>
 
-<div class="conteneurBloc n2"></br>
+<div class="conteneurBloc n3">
   <h2>
   Liste de vos maisons
   </h2>
-</div>
   <div class="conteneurBloc n2 right">
     <p class=center>
       <a class="boutton" href ="monespace.php?cible=monespace/mamaison.php" >ajouter/supprimer des maisons</a>
     </p>
   </div>
   <?php include('Vue/Listes/listemaisons.php');?>
+</div>
 
 
-<div class="conteneurBloc n2"><br>
+
+<div class="conteneurBloc n3"><br>
   <h2>
     Liste de vos pièces
   </h2>
-</div>
   <div class="conteneurBloc n2 right">
     <p class=center>
       <a class="boutton" href ="monespace.php?cible=monespace/piece.php" >ajouter/supprimer des pièces</a>
     </p>
   </div>
-  <?php include('Vue/Listes/listepieces.php');?>
+    <?php include('Vue/Listes/listepieces.php');?>
+</div>
 
 
 
-<div class="conteneurBloc n2"><br>
+
+
+<div class="conteneurBloc n3"><br>
   <h2>
     Liste de vos capteurs
   </h2>
+  <div class="conteneurBloc n2 right">
+    <p class=center>
+      <a class="boutton" href ="monespace.php?cible=monespace/ajoutcapteurs.php" >ajouter/supprimer des capteurs</a>
+    </p>
+  </div>
+    <?php include('Vue/Listes/listecapteurs.php');?>
 </div>
-<div class="conteneurBloc n2 right">
-  <p class=center>
-    <a class="boutton" href ="monespace.php?cible=monespace/ajoutcapteurs.php" >ajouter/supprimer des capteurs</a>
-  </p>
-</div>
-  <?php include('Vue/Listes/listecapteurs.php');?>
 
-  <div class="conteneurBloc n2"><br>
+
+
+  <div class="conteneurBloc n3"><br>
     <h2>
       Liste de vos effecteurs
     </h2>
-  </div>
-  <div class="conteneurBloc n2 right">
-    <p class=center>
-      <a class="boutton" href ="monespace.php?cible=monespace/ajoutcapteurs.php" >ajouter/supprimer des effecteurs</a>
-    </p>
-  </div>
+    <div class="conteneurBloc n2 right">
+      <p class=center>
+        <a class="boutton" href ="monespace.php?cible=monespace/ajoutcapteurs.php" >ajouter/supprimer des effecteurs</a>
+      </p>
+    </div>
     <?php include('Vue/Listes/listeeffecteur.php');?>
+  </div>
